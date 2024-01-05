aa0700550f5616c80c5f61f5af4fb756bab7e933
Jessica Townsend <shiny.robot.death@gmail.com>
2024-01-06 00:35:46 +1100

    added the about us page and content

--- app/views/content/about/index.php
+++ app/views/content/about/index.php
@@ -1,7 +1,86 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            hello world!
+<div id="particles-js" class="title-header align-bottom">
+    <div class="container-fluid position-absolute">
+        <div class="row">
+            <div class="col-md-12 text-center text-white">
+                <img src="/public/img/aboutus.png" alt="About Us" class="img-fluid section-title">
+            </div>
         </div>
     </div>
 </div>
+
+<div class="title-spacer"></div>
+
+<style>
+    body {
+        font-size: 100%;
+    }
+
+    li {
+        margin-bottom: 20px;
+    }
+</style>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <img src="/public/img/everyone.png" alt="Everyone's Welcome!" class="img-fluid d-none d-md-block" style="margin-top: -120px; margin-bottom: 32px; ">
+            <p>
+                Wvnder is a bathhouse and lounge 'bathlounge', and bar in Lamia on the Primal datacenter!
+                We seek to be a comfy and welcoming space for all who enjoy cozy, more intimate spaces!
+                With a dreamy-like ambience, we hope you come 'wvnder' with us!
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <h3>Venue Features</h3>
+            <p>
+                Wvnder's current location features:
+            </p>
+            <ul>
+                <li>Bar with menu items (during hosted hours!)</li>
+                <li>Five baths, including two private baths</li>
+                <li>Lounge area</li>
+            </ul>
+        </div>
+
+        <div class="col-sm-6">
+            <h3>Venue Rules</h3>
+            <p>
+                In the interest of maintaining a comfortable and welcoming space for all,
+                we ask that all patreons abide by the following rules.
+            </p>
+            <div class="alert alert-danger">
+                <p>
+                    Failure to follow the rules may result in:
+                    <ul>
+                        <li>being asked to leave the premises, and/or;</li>
+                        <li>
+                            being blacklisted from Wvnder<br>
+                            <small>
+                                (and subsequently reported to GMs if the blacklist is ignored)
+                            </small>
+                        </li>
+                    </ul>
+                </p>
+            </div>
+            <p>
+                <ul>
+                    <li>Be kind and respect others - treat others how you'd wish to be treated</li>
+                    <li>Wvnder is a "SFW upon arrival" venue. This means that ERP/NSFW activites be relegated to strictly non-public chat.</li>
+                    <li>
+                        Nudity is allowed within the venue (vanilla or otherwise) but with an exception:
+                        <br>
+                        <strong>Absolutely no NSFW Lalafell/portrayals of minors</strong>
+                        <br>
+                        Violation of this rule will result in being asked to leave.
+                    </li>
+                </ul>
+            </p>
+        </div>
+    </div>
+</div>
+
+<script src="/public/js/particles.js"></script>
+<script src="/public/js/app.js?<?=uniqid()?>"></script>
