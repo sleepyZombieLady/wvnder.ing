cd6fcf7f0e19d343f4bf247529a9ca60d06e52fa
Jessica Townsend <shiny.robot.death@gmail.com>
2024-01-05 15:36:18 +1100

    updated positioning of logo

--- app/views/_layouts/default.php
+++ app/views/_layouts/default.php
@@ -39,12 +39,13 @@
         </div>
     </nav>
 
-    <div id="particles-js">
-        <div class="branding">
-            <img class="img-fluid" src="/public/img/logo.png" alt="WVNDER logo"><br>
-            <img class="img-fluid" src="/public/img/address.png" alt="address">
-        </div>
+
+    <div class="branding">
+        <img class="img-fluid" src="/public/img/logo.png" alt="WVNDER logo"><br>
+        <img class="img-fluid" src="/public/img/address.png" alt="address">
     </div>
+
+    <div id="particles-js"></div>
 
     <script src="/public/js/particles.js"></script>
     <script src="/public/js/app.js?<?=uniqid()?>"></script>
